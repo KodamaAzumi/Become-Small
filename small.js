7d3f9280fd1c2359700c84a572f4ae2f077c8413
DESKTOP-EPTIJJL\azumi2020si <azumi2020si@nzu.jp>
2022-04-30 05:52:48 +0900

    #鬼滅をボボボーボ・ボーボボにする拡張機能

--- small.js
+++ small.js
@@ -1,1 +1,16 @@
 'use script';
+let buttonElement = document.getElementById('button');
+function kimetu() {
+    document.body.innerHTML=document.body.innerHTML.replace(/鬼滅の刃/g, 'ボボボーボ・ボーボボ');
+}
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    buttonElement.addEventListener('click', (event) => {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({
+                target : {tabId: tabs[0].id},
+                func : kimetu,
+            });
+        }); 
+    });
+});
