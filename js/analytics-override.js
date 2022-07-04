9d03be5f3dacea7b05660e8b96410f39a3cfda99
DESKTOP-EPTIJJL\azumi2020si <azumi2020si@nzu.jp>
2022-07-05 05:39:29 +0900

    #GAを抜いた

--- js/analytics-override.js
+++
@@ -1,3 +0,0 @@
-var _gaq = _gaq || [];
-_gaq.push(["_setAccount", "G-HQ91FT3VT4"]);
-_gaq.push(["_trackPageview", "../override.html"]);
--- js/override.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){    
-    const scriptTag = document.createElement("script");
-    scriptTag.src = chrome.runtime.getURL("analytics-override.js");
-    scriptTag.type = "text/javascript";
-    document.head.appendChild(scriptTag);
-}
